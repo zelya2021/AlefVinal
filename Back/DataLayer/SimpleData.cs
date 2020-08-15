2b459ace8602ba4ca0d968968fc0af28dc08fc29
zelya2021 <zelenckina1999@gmail.com>
2020-08-15 17:58:01 +0300

    Finish Data Access Layer Try2

--- Back/DataLayer/SimpleData.cs
+++ Back/DataLayer/SimpleData.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DataLayer
 {
-    class SimpleData
+    public static class SimpleData
     {
+        public static void InitData(EFDBContext context)
+        {
+            if (!context.Codes.Any())
+            {
+                context.Codes.Add(new Entityes.Code() { Id = 1, Value = 145, Name="Код" });
+                context.Codes.Add(new Entityes.Code() { Id = 2, Value = 783, Name = "Водафон" });
+                context.SaveChanges();
+            }
+        }
     }
 }
