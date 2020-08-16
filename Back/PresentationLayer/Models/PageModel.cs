179be02c85f7c7160b34e9e425d8fc3869b6cb2e
zelya2021 <zelenckina1999@gmail.com>
2020-08-16 21:39:27 +0300

    Added Page and PageModels

---
+++ Back/PresentationLayer/Models/PageModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PresentationLayer.Models
+{
+    public class PageViewModel { }
+    public class PageEditModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Html { get; set; }
+    }
+}
